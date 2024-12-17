7546a1d7f286267e76ad030edeb31b01a8b886f8
Sobia Riaz <167880119+sobiariazzz@users.noreply.github.com>
2024-12-17 11:14:08 -0800

    Add files via upload

---
+++ readmore/app.js
@@ -0,0 +1,49 @@
+let extracontent=document.getElementById("extracontent");
+let readmorelink=document.getElementById("readmorelink");
+let extracontent2=document.getElementById("extracontent2");
+let readmorelink2=document.getElementById("readmorelink2");
+let extracontent3=document.getElementById("extracontent3");
+let readmorelink3=document.getElementById("readmorelink3");
+let extracontent4=document.getElementById("extracontent4");
+let readmorelink4=document.getElementById("readmorelink4");
+
+
+
+
+readmorelink.onclick=()=>{
+ if( extracontent.style.display==="none"){
+    extracontent.style.display="inline";
+    readmorelink.textContent="read-less";
+}else{
+ 
+        extracontent.style.display="inline";
+        readmorelink.textContent="readless";
+    
+}
+};
+readmorelink2.onclick=()=>{
+    if( extracontent2.style.display==="none"){
+       extracontent2.style.display="inline";
+       readmorelink2.textContent="read-less";
+   }else{
+    
+           extracontent2.style.display="inline";
+           readmorelink2.textContent="readless";
+       
+   }
+   };
+   readmorelink3.onclick=()=>{
+    if( extracontent3.style.display==="none"){
+       extracontent3.style.display="inline";
+       readmorelink3.textContent="read-less";
+   }else{
+    
+           extracontent3.style.display="inline";
+           readmorelink3.textContent="readless";
+       
+   }
+   };
+  
+         
+
+    
